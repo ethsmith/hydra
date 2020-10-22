80e68b7b159207a8a320dfbc132d84d2132e062c
ethsmith <ethan.smith1845@gmail.com>
2020-10-21 22:13:58 -0500

    Add objective tracking

--- src/main/java/org/dragonetmc/hydra/GameManager.java
+++ src/main/java/org/dragonetmc/hydra/GameManager.java
@@ -2,23 +2,26 @@ package org.dragonetmc.hydra;
 
 import org.dragonetmc.hydra.game.GameState;
 import org.dragonetmc.hydra.level.Level;
+import org.dragonetmc.hydra.objective.Objective;
 import org.dragonetmc.hydra.team.*;
 import org.dragonetmc.hydra.util.AnnotationUtil;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class GameManager {
 
     private static final Map<String, Method> gameStatesById = new HashMap<>();
     private static final Map<Integer, Method> gameStatesByPriority = new HashMap<>();
     private static final List<Team> teams = new LinkedList<>();
+    private static final Set<Objective> objectives = new HashSet<>();
     private static String gameState = "initialize";
     private static Level level;
+
+    private static final ScheduledExecutorService objectiveTracker = Executors.newScheduledThreadPool(1);
 
     public static String getGameState() {
         return gameState;
@@ -50,6 +53,15 @@ public class GameManager {
 
     public static List<Team> getTeams() {
         return teams;
+    }
+
+//    public static Set<Objective> getObjectives() {
+//        return objectives;
+//    }
+
+    public static void addObjective(Objective objective) {
+        objectives.add(objective);
+        objectiveTracker.scheduleAtFixedRate((Runnable) objective::execute, 500, 500, TimeUnit.MILLISECONDS);
     }
 
     public static Level getLevel() {
