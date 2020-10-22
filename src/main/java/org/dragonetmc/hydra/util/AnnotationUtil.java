171788815380aad9eda84a4d5f8692010516f2eb
ethsmith <ethan.smith1845@gmail.com>
2020-10-22 13:34:44 -0500

    Update reflections library

--- src/main/java/org/dragonetmc/hydra/util/AnnotationUtil.java
+++ src/main/java/org/dragonetmc/hydra/util/AnnotationUtil.java
@@ -12,7 +12,6 @@ import org.reflections.util.ConfigurationBuilder;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
