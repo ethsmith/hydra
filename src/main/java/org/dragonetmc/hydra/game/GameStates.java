1e45d22c436bfed877a2b2e5dbc695273f37877e
ethsmith <ethan.smith1845@gmail.com>
2020-10-23 16:30:13 -0500

    Debugging lack of annotation processing 2

--- src/main/java/org/dragonetmc/hydra/game/GameStates.java
+++ src/main/java/org/dragonetmc/hydra/game/GameStates.java
@@ -2,10 +2,6 @@ package org.dragonetmc.hydra.game;
 
 import org.atteo.classindex.IndexAnnotated;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target(ElementType.TYPE)
 @IndexAnnotated
 public @interface GameStates {
     String[] states();
--- src/main/java/org/dragonetmc/hydra/team/Party.java
+++ src/main/java/org/dragonetmc/hydra/team/Party.java
@@ -2,10 +2,6 @@ package org.dragonetmc.hydra.team;
 
 import org.atteo.classindex.IndexAnnotated;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target(ElementType.TYPE)
 @IndexAnnotated
 public @interface Party {
     int min();
