30ca8beb1ea7243527af7a65d6ca533e5e3b66d2
ethsmith <ethan.smith1845@gmail.com>
2020-11-05 16:37:18 -0600

    Add auto update method to Scoreboard

--- src/main/java/org/dragonetmc/hydra/scoreboard/Scoreboard.java
+++ src/main/java/org/dragonetmc/hydra/scoreboard/Scoreboard.java
@@ -1,17 +1,43 @@
 package org.dragonetmc.hydra.scoreboard;
 
 import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitTask;
+import org.dragonetmc.hydra.GameManager;
 
 public abstract class Scoreboard {
 
     @Getter
     private final String name;
 
+    @Getter
+    private boolean autoUpdate = false;
+
+    @Getter
+    @Setter
+    private int updateInterval = 10;
+
+    @Getter
+    private BukkitTask updateTask = null;
+
     public Scoreboard(String name) {
         this.name = name;
+        setAutoUpdate(true);
     }
 
     public abstract void show();
 
     public abstract void hide();
+
+    public void setAutoUpdate(boolean autoUpdate) {
+        this.autoUpdate = autoUpdate;
+        Plugin plugin = GameManager.getPlugin();
+
+        if (updateTask != null)
+            updateTask.cancel();
+
+        if (autoUpdate)
+            updateTask = plugin.getServer().getScheduler().runTaskTimer(plugin, this::show, updateInterval, updateInterval);
+    }
 }
